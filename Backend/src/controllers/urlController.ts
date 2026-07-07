992f4d62d22318c7e29adc5e6b43a8b4e4c7650c
Konstantin Sl <kostatop2@gmail.com>
2026-07-07 16:32:27 +0300

    feat:added urls creation

--- Backend/src/controllers/urlController.ts
+++ Backend/src/controllers/urlController.ts
@@ -1,7 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import { urlRepository } from "../repositories/urlRepository";
+import { hash, randomInt } from "node:crypto";
+import { Base62 } from "@sindresorhus/base62";
 
-const getBaseUrl=async (req: Request, res: Response, next: NextFunction) => {
+const encodeUrl = async(baseUrl: string) => {
+    while(true){
+        let hashed = BigInt("0x" + hash('sha256', baseUrl + Date.now() + randomInt(10000000000)).substring(0, 12))
+        const base62 = new Base62();
+        let shortUrl = base62.encodeBigInt(hashed);
+        let statsUrl = base62.encodeBigInt(hashed + BigInt(Date.now()));
+        const colisionExists=await urlRepository.checkCollision(shortUrl,statsUrl);
+        if(!colisionExists){
+            return {shortUrl,statsUrl}
+        }
+    }
+}
+
+const getBaseUrl = async (req: Request, res: Response, next: NextFunction) => {
     const shortUrl = req.query.shortUrl;
     if (typeof shortUrl !== "string")
         res.status(400).json({
@@ -18,7 +33,7 @@ const getBaseUrl=async (req: Request, res: Response, next: NextFunction) => {
         res.status(404)
     }
 }
-const getStatistics=(req: Request, res: Response) => {
+const getStatistics = (req: Request, res: Response) => {
     const statsUrl = req.query.statsUrl;
     if (typeof statsUrl !== "string")
         res.status(400).json({
@@ -30,7 +45,7 @@ const getStatistics=(req: Request, res: Response) => {
         res.status(201).json([])
     }
 }
-const createUrl=(req: Request, res: Response) => {
+const createUrl = async(req: Request, res: Response) => {
     const baseUrl = req.query.baseUrl;
     if (typeof baseUrl !== "string")
         res.status(400).json({
@@ -38,12 +53,13 @@ const createUrl=(req: Request, res: Response) => {
             message: 'Bad request'
         });
     else {
-        urlRepository.createUrls("", "", "");
+        const {shortUrl,statsUrl}=await encodeUrl(baseUrl)
+        urlRepository.createUrls(baseUrl, shortUrl, statsUrl);
         res.status(201).json({
-            shortUrl: "",
-            statisticsUrl: ""
+            shortUrl: shortUrl,
+            statisticsUrl: statsUrl
         })
     }
 }
 
-export {getBaseUrl,getStatistics,createUrl}
+export { getBaseUrl, getStatistics, createUrl }
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 import { urlRepository } from "./repositories/urlRepository";
 import "./routes/urlRoutes"
-urlRepository.getLongUrl("")
+
 
 app.listen(3000)
